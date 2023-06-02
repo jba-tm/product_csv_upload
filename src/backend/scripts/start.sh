08b2a176b0a53bbebdbf1229249cb93b43068ff8
Your Name <you@example.com>
2023-06-03 00:51:20 +0500

    application

--- src/backend/scripts/start.sh
+++ src/backend/scripts/start.sh
@@ -4,12 +4,12 @@
 set -e
 
 if [ -f ./app/main.py ]; then
-    DEFAULT_MODULE_NAME=app.main
+    DEFAULT_MODULE_NAME=backend.wsgi
 elif [ -f ./main.py ]; then
-    DEFAULT_MODULE_NAME=main
+    DEFAULT_MODULE_NAME=wsgi
 fi
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-VARIABLE_NAME=${VARIABLE_NAME:-app}
+VARIABLE_NAME=${VARIABLE_NAME:-application}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
 if [ -f ./app/gunicorn_conf.py ]; then
